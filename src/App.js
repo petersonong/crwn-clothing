ae6ceab95ac72a65467f924d441eb92782edc614
Peterson Ong <peterson.ong@gmail.com>
2019-10-20 12:12:53 +0800

    Created an App via create-react-App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './components/homePage'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
